7882c17ac9b2c393d1694a0da7f624402e09caee
Ike <ike@tura-home.de>
2012-11-21 18:43:40 +0100

    Rescaled inventory values

--- firmware/minGame/src/Player.c
+++ firmware/minGame/src/Player.c
@@ -6,23 +6,23 @@
 ////// ITEM DEFINITIONS ////////////
 
 
-//                      INVTA,SPD,SPW,MDM,INV,  PRZ
-Item shoesStart =       {   1,  0,  0,  0,  0,      0, "Start Shoes", "The Shoes you start with."};
-Item pickaxeStart =     {   1,  2, -1,  0,  0,      0, "Start Pickaxe", "The Pickace you start with."};
-Item backpackStart =    {   0, -1, -1,  0,  2,      0, "Start Backpack", "The Backpack you start with."};
+//                       INVTA, SPD, SPW, MDM,   INV,    PRZ,   NAME,               DESC
+Item shoesStart =       {  100,   0,   0,   0,     0,      0,   "Start Shoes",      "The Shoes you start with."};
+Item pickaxeStart =     {  100,   2,  -1,   0,     0,      0,   "Start Pickaxe",    "The Pickace you start with."};
+Item backpackStart =    {    0,  -1,  -1,   0,   200,      0,   "Start Backpack",   "The Backpack you start with."};
 
-Item shoesSlow =        {   2,  1,  3,  1,  0,    300, "Slow Shoes", "Fairly slow shoes. They speed you up somewhat."};
-Item shoesFast =        {   4,  2,  5,  2,  0,    700, "Fast Shoes", "Quite good shoes you might want to take for a walk."};
-Item shoesTurbo =       {   7,  3,  9,  3,  0,   1100, "Turbo Shoes", "Very good shoes that make you feel incredibly fast."};
+Item shoesSlow =        {  200,   1,   3,   1,     0,    300,   "Slow Shoes",       "Fairly slow shoes. They speed you up somewhat."};
+Item shoesFast =        {  400,   2,   5,   2,     0,    700,   "Fast Shoes",       "Quite good shoes you might want to take for a walk."};
+Item shoesTurbo =       {  700,   3,   9,   3,     0,   1100,   "Turbo Shoes",      "Very good shoes that make you feel incredibly fast."};
 
-Item pickaxeSlow =      {   3,  4, -2,  2,  0,    400, "Slow Pickaxe", "A weak Pickaxe. There are better ones."};
-Item pickaxeFast =      {   5,  7, -3,  4,  0,    700, "Fast Pickaxe", "Quite good pickaxe. It gets the job done well."};
-Item pickaxeTurbo =     {   9, 11, -4,  7,  0,   1300, "Turbo Pickaxe", "Awesomely fast pickaxe. You're blazing through rock."};
+Item pickaxeSlow =      {  300,   4,  -2,   2,     0,    400,   "Slow Pickaxe",     "A weak Pickaxe. There are better ones."};
+Item pickaxeFast =      {  500,   7,  -3,   4,     0,    700,   "Fast Pickaxe",     "Quite good pickaxe. It gets the job done well."};
+Item pickaxeTurbo =     {  900,  11,  -4,   7,     0,   1300,   "Turbo Pickaxe",    "Awesomely fast pickaxe. You're blazing through rock."};
 
-Item backpackSmall =    {   0, -2, -2,  1,  8,    500, "Small Backpack", "It can barely hold more than your equiptment."};
-Item backpackMedium =   {   0, -3, -3,  3, 16,    900, "Medium Backpack", "It should get you through a few minutes of mining."};
-Item backpackBig =      {   0, -4, -4,  6, 26,   1200, "Big Backpack", "This backpack should hold all you'd ever want to carry."};
-Item backpackHuge =     {   0, -5, -5, 10, 33,   1900, "Huge Backpack", "This backpack can probably hold more minerals than our world has."}:
+Item backpackSmall =    {    0,  -2,  -2,   1,   800,    500,   "Small Backpack",   "It can barely hold more than your equiptment."};
+Item backpackMedium =   {    0,  -3,  -3,   3,  1600,    900,   "Medium Backpack",  "It should get you through a few minutes of mining."};
+Item backpackBig =      {    0,  -4,  -4,   6,  2600,   1200,   "Big Backpack",     "This backpack should hold all you'd ever want to carry."};
+Item backpackHuge =     {    0,  -5,  -5,  10,  3300,   1900,   "Huge Backpack",    "This backpack can probably hold more minerals than our world has."}:
 
 
 ////////////////////////////////////
